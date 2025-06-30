2b030ea1b62e0d7589df9bbc99c36ac70803d954
Kevin Kamto <kamtokevin@gmail.com>
2025-06-30 15:06:59 -0400

    feat: created config file

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+
+interface DegenlendConfig {
+    apiBaseUrl?: string;
+}
+
+let config: DegenlendConfig = {
+    apiBaseUrl: 'https://api.degenlend.com',
+};
+
+export function getConfig(): DegenlendConfig {
+    return config;
+}
+
+export function setConfig(newConfig: DegenlendConfig): void {
+    config = {
+        ...config,
+        ...newConfig,
+    };
+}
